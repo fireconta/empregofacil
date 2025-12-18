a1f048b3e31cb602ccfb966ab8ea68baa80c3249
fireconta <fireconta02@gmail.com>
2025-12-18 15:54:06 -0300

    Add files via upload

---
+++ client/src/config.ts
@@ -0,0 +1,2 @@
+export const APP_DOWNLOAD_LINK = "https://play.google.com/store/apps/details?id=com.empregofacil.app"; // Link para download do aplicativo definido aqui
+export const GOOGLE_SCRIPT_URL = ""; // Cole a URL do seu Web App do Google Apps Script aqui
