2eb1f224f2ad8b135b0909e11a3b5d0391de67b9
fireconta <fireconta02@gmail.com>
2025-12-18 15:49:15 -0300

    Delete client directory

--- client/src/consts.ts
+++
@@ -1,8 +0,0 @@
-export const WHATSAPP_NUMBER = "62995771104";
-export const WHATSAPP_LINK_BASE = "https://wa.me/5562995771104";
-
-export const WHATSAPP_SUPPORT_MESSAGE = "Olá, preciso de suporte sobre o link de download do aplicativo que está em manutenção.";
-export const WHATSAPP_COMPANY_MESSAGE = "Olá, sou uma empresa e gostaria de cadastrar uma vaga gratuitamente. Somos PF/PJ.";
-
-export const WHATSAPP_SUPPORT_LINK = `${WHATSAPP_LINK_BASE}?text=${encodeURIComponent(WHATSAPP_SUPPORT_MESSAGE)}`;
-export const WHATSAPP_COMPANY_LINK = `${WHATSAPP_LINK_BASE}?text=${encodeURIComponent(WHATSAPP_COMPANY_MESSAGE)}`;
