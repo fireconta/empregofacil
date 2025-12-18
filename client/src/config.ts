10a9e93e93147b11d9e8880244165db6254540fd
fireconta <fireconta02@gmail.com>
2025-12-18 08:38:29 -0300

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
 export const APP_DOWNLOAD_LINK = "https://play.google.com/store/apps/details?id=com.empregofacil.app"; // Link para download do aplicativo definido aqui
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz_zKIVpi1pTFoLt9amtiy7YNiW8QgnjQdEo0_QIjg/dev"; // Cole a URL do seu Web App do Google Apps Script aqui
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzM0VL59R0QYPfqH0WhOy2K-fS-UOXvhGP6l4mjo_KlovTGPb4C13GOaeUulhaqy8T/exec"; // Cole a URL do seu Web App do Google Apps Script aqui
