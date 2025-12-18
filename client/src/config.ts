1c5b73ed8b1b724de51217eee6e1646305eb4f4c
fireconta <fireconta02@gmail.com>
2025-12-18 10:25:36 -0300

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const APP_DOWNLOAD_LINK = "https://play.google.com/store/apps/details?id=
 
 // URL do Script do Google Sheets (API)
 // Substitua pela URL gerada no Google Apps Script (Implantar > App da Web)
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzM0VL59R0QYPfqH0WhOy2K-fS-UOXvhGP6l4mjo_KlovTGPb4C13GOaeUulhaqy8T/exec";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyIpVlnnJ50fihzAfEibOPTtpvET2rGDapwm-de_kaOhiFtmkRXsGJgRl00-hlP29BB/exec";
 
 // Mensagem de Compartilhamento Personalizada
 export const SHARE_MESSAGE = {
